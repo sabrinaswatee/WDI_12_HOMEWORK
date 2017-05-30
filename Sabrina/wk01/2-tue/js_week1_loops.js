1112e60c4c9c5a854397eb8618ba4dc1c70d4fa1
sabrinaswatee <sabrina.swatee@gmail.com>
2017-05-30 16:21:12 +1000

    added-js-part2

--- Sabrina/wk01/2-tue/js_week1_loops.js
+++ Sabrina/wk01/2-tue/js_week1_loops.js
@@ -21,3 +21,21 @@ var suffix = ['1st', '2nd', '3rd'];
 for (var no = 1; no <= top_choices.length; no++) {
   console.log("My " + suffix[no - 1] + " choice is " + top_choices[no - 1]);
 }
+
+//js-day-else-if
+
+var d = new Date();
+var currentYear = d.getFullYear();
+var year = Number(prompt("Enter year"));
+
+if (year == currentYear) {
+  console.log("I'm in the present!");
+}
+else {
+  if (year < currentYear) {
+    console.log("Whoa!Blast from the past!");
+  }
+  else if (year > currentYear) {
+    console.log("Greetings from the future!");
+  }
+}
