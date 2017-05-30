c0f49677b2193ff86fc814a9e0e90871f6fbb745
sabrinaswatee <sabrina.swatee@gmail.com>
2017-05-30 15:24:42 +1000

    added js-hw

---
+++ Sabrina/wk01/2-tue/js_week1_loops.js
@@ -0,0 +1,23 @@
+//The even/odd reporter
+
+for (var count = 0; count <= 20; count++) {
+  if (count % 2 == 0) {
+      console.log(count + " is even");
+  }
+}
+
+// Multiplication table
+
+for (var i = 1; i <= 10; i++) {
+  for (var j = 0; j <= 10; j++) {
+    console.log(i + " * " + j, " = ", i * j);
+  }
+}
+
+// Your top choices
+
+var top_choices = ['Blue', 'Green', 'Beige'];
+var suffix = ['1st', '2nd', '3rd'];
+for (var no = 1; no <= top_choices.length; no++) {
+  console.log("My " + suffix[no - 1] + " choice is " + top_choices[no - 1]);
+}
