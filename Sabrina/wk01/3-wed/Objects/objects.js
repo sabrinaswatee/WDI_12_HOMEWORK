a71cab6906d74efe4aceabfcc353fdc086bc272e
sabrinaswatee <sabrina.swatee@gmail.com>
2017-06-01 14:45:43 +1000

    added checkpoint week 1

--- Sabrina/wk01/3-wed/Objects/objects.js
+++ Sabrina/wk01/3-wed/Objects/objects.js
@@ -9,6 +9,9 @@ var recipeCard = {
 console.log(recipeCard.title);
 console.log("Serves: " + recipeCard.serving);
 console.log("Ingredients:");
+
+/* console.log(recipeCard.ingredients.join('\n\u2022')) can replace the for loop */
+
 for (var no = 0; no < recipeCard.ingredients.length; no++) {
   console.log('\u2022 ' + recipeCard.ingredients[no]);
 }
@@ -37,6 +40,10 @@ var readingList = [
 var listReading = function () {
   for (var count = 0; count < readingList.length; count++) {
     console.log(readingList[count].title + " by " + readingList[count].author);
+
+/* can replace below line with if (readingList[count].alreadyRead) {
+ its a boolean with true/false options, no need to check whether == true or not */
+
     if (readingList[count].alreadyRead == true) {
       console.log('You have already read "' + readingList[count].title + '" by ' + readingList[count].author);
     }
@@ -55,6 +62,14 @@ var movieDatabase = {
   duration: 120,
   stars: ['Dev Patel', ' Nicole Kidman', ' Rooney Mara']
 }
+
+/* It is better to keep functions conscise and return something
+ Allow the user to print it or use in other methods - more versatile
+ var listMovie = function (film) {
+   return (film.title + " lasts for " + film.duration + " minutes. Stars: " + film.stars.join(',') + '.');
+ };
+ var result = listMovie(movieDatabse);
+ console.log(result); */
 
 var listMovie = function () {
   console.log(movieDatabase.title + " lasts for " + movieDatabase.duration + " minutes. Stars: " + movieDatabase.stars + '.');
---
+++ Sabrina/wk01/4-thu/week1_checkpoints/week1_checkpoints.js
@@ -0,0 +1,41 @@
+// JS Variables
+
+var captain = "Jack";
+var phrase = "Oh ".concat(captain.concat(", my ".concat(captain.concat("!"))));
+
+//
+// JS Conditionals
+
+var souls = 3;
+var lifeRafts = 2;
+
+if (souls > lifeRafts) {
+  console.log("SOS!");
+}
+
+//
+// Data Structures - JS Arrays
+
+var weekend = ['Saturday'];
+weekend.push('Sunday');
+weekend.unshift('Friday');
+var day = weekend[1];
+weekend.splice(0,1);
+
+//
+// Data Structures - JS Objects
+
+var brain = {
+  energyLevel: 10
+}
+var energy = brain.energyLevel;
+
+//
+// JS Functions
+
+var rectangleArea = function (length, width) {
+  return length * width;
+}
+rectangleArea (3, 4);
+
+//
