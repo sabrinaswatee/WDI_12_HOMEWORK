93c4bb5bd9eca95a91e1bc134559107d88052f66
sabrinaswatee <sabrina.swatee@gmail.com>
2017-06-02 16:47:02 +1000

    final version

--- Sabrina/wk01/4-thu/PT_Planner/melb_metro_ptv_mta.js
+++ Sabrina/wk01/4-thu/PT_Planner/melb_metro_ptv_mta.js
@@ -1,8 +1,13 @@
 // Melbourne Public Transport Journey Planner
 
 /*
-Array of Objects on train lines
+Array of station list and Array of Objects on train lines
 */
+var stationList = ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie',
+                   'Flagstaff', 'Melbourne Central', 'Parliament', 'Kooyong', 'Tooronga', 'Southern Cross',
+                   'South Yarra', 'Prahran', 'Windsor','Auburn', 'Surrey Hills', 'Blackburn', 'Ringwood',
+                   'Lilydale', 'Caulfield', 'Oakleigh', 'Noble Park', 'Yarraman', 'Hallam', 'Pakenham',
+                   'Werribee', 'Newport', 'Malvern', 'Patterson', 'Chelsea', 'Frankston'];
 var trainLines = [
   {
     name: 'Alamein',
@@ -15,13 +20,25 @@ var trainLines = [
   {
     name: 'Sandringham',
     stations: ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
+  },
+  {
+    name: 'Lilydale',
+    stations: ['Richmond', 'Auburn', 'Surrey Hills', 'Blackburn', 'Ringwood', 'Lilydale']
+  },
+  {
+    name: 'Pakenham',
+    stations: ['Richmond', 'Caulfield', 'Oakleigh', 'Noble Park', 'Yarraman', 'Hallam', 'Pakenham']
+  },
+  {
+    name: 'Frankston',
+    stations: ['Werribee', 'Newport', 'Richmond', 'Malvern', 'Patterson', 'Chelsea', 'Frankston']
   }
 ]
 
 /*
 Starting the journey planner
 */
-alert ('Melbourne Public Transport Journey Planner');
+alert ('Welcome to Melbourne Public Transport Journey Planner');
 
 /*
 Prints out origin and destination
@@ -75,6 +92,7 @@ var findIndexUnmerged = function (origin, destination, line) {
   }
   var stop = stops (beginning, ending);
   console.log (route + '\n' + stop);
+  // alert ('Thank you for using the service!');
 }
 
 /*
@@ -93,16 +111,14 @@ var findIndexMerged = function (origin, destination, startLine, endLine) {
 Slices the section of route from the line and returns as a joined string
 */
 var forwardRoute = function (beginning, ending, line) {
-  var path = trainLines[line].stations.slice(beginning, ending + 1);
-  return (path.join(' -----> '));
+  return trainLines[line].stations.slice(beginning, ending + 1).join(' -----> ');
 }
 
 /*
 Slices the section of route from the line, reverses it and returns as a joined string
 */
 var backwardRoute = function (beginning, ending, line) {
-  var path = trainLines[line].stations.slice(beginning, ending + 1).reverse();
-  return (path.join(' -----> '));
+  return trainLines[line].stations.slice(beginning, ending + 1).reverse().join(' -----> ');
 }
 
 /*
@@ -145,6 +161,7 @@ var mergedStops = function (start, stop, changeOne, changeTwo) {
   var stopsOne = Math.abs(changeOne - start);
   var stopsTwo = Math.abs(changeTwo - stop);
   console.log ((stopsOne + stopsTwo) + ' stops total');
+  // alert ('Thank you for using the service!');
 }
 
 /*
@@ -152,7 +169,13 @@ Prompts user input for origin and destination and calls function
 ?? Iterates if user inputs anything other than station names ??
 */
 var origin = prompt("What's your origin?");
+while (!stationList.includes(origin)) {
+  var origin = prompt("What's your origin?");
+}
 var destination = prompt("What's your destination?");
+while (!stationList.includes(destination)) {
+  var destination = prompt("What's your destination?");
+}
 travelPlanner (origin, destination);
-
+alert ('Thank you for using the service!');
 //
