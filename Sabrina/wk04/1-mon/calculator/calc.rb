20778868f08a488af7774d73b68e63a068164536
sabrinaswatee <sabrina.swatee@gmail.com>
2017-06-20 08:43:35 +1000

    arrays

--- Sabrina/wk04/1-mon/calculator/calc.rb
+++ Sabrina/wk04/1-mon/calculator/calc.rb
@@ -1,27 +1,98 @@
 =begin
 Explanation
+You will be building a calculator. A calculator can perform multiple arithmetic operations.
+Your function should allow the user to choose which operation is expected,
+enter in the values to perform the operation on, and ultimately view the result.
 
-You will be building a calculator. A calculator can perform multiple arithmetic operations. Your function should allow the user to choose which operation is expected, enter in the values to perform the operation on, and ultimately view the result.
 Specification:
-
 A user should be given a menu of operations
 A user should be able to choose from the menu
 A user should be able to enter numbers to perform the operation on
 A user should be shown the result
 This process should continue until the user selects a quit option from the menu
-Phase 1
 
+Phase 1
 Calculator functionality
 Calculator should be able to do basic arithmetic (+,-, *, /)
-Phase 2
 
+Phase 2
 Advanced Calculator functionality
 Calculator should be able to do basic arithmetic (exponents, square roots)
-Phase 3
 
+Phase 3
 User should be given a menu of Calculator functionality
 User should be able to choose intended functionality
-Optional Extensions
 
+Optional Extensions
 Does your calculator allows addition of more then 2 numbers in one go? eg: 2 + 3 + 4
 =end
+
+def sum (num_array)
+  result = 0
+  num_array.each do |num|
+    result += num.to_i
+  end
+  print "Total sum = #{result}\n"
+end
+
+def difference (num1, num2)
+  print "Difference of #{num1} and #{num2} = #{num1 - num2}\n"
+end
+
+def product (num1, num2)
+  print "Product of #{num1} and #{num2} = #{num1 * num2}\n"
+end
+
+def quotient (num1, num2)
+  print "Quotient of #{num1} divided by #{num2} = #{num1 / num2}\n"
+end
+
+def modulo (num1, num2)
+  print "Remainder of #{num1} divided by #{num2} = #{num1 % num2}\n"
+end
+
+def power (num1, num2)
+  print "Value of #{num1} to the power of #{num2} = #{num1 ** num2}\n"
+end
+
+def root (num)
+  print "Square root of #{num} = #{num ** 0.5}\n"
+end
+
+def select_operation (operation, input1, input2)
+  case operation
+  when 2
+    difference(input1, input2)
+  when 3
+    product(input1, input2)
+  when 4
+    quotient(input1, input2)
+  when 5
+    modulo(input1, input2)
+  when 6
+    power(input1, input2)
+  end
+end
+
+puts "Welcome to Command Line Calculator"
+loop do
+  puts "Select from \n1. Addition \n2. Subtraction \n3. Mutiplication \n4. Division \n5. Remainder \n6. Power \n7. Square Root \n8. Quit"
+  operation = gets.chomp.to_i
+  if operation == 1
+    print "Inputs (separated by commas): "
+    input_array = gets.chomp.split(', ')
+    puts input_array
+    sum(input_array)
+  elsif operation > 1 && operation < 7
+    print "Input 1: "
+    input1 = gets.chomp.to_i
+    print "Input 2: "
+    input2 = gets.chomp.to_i
+    select_operation(operation, input1, input2)
+  elsif operation == 7
+    puts "Input: "
+    input = gets.chomp.to_i
+    root(input)
+  end
+  break if operation == 8
+end
--- Sabrina/wk04/1-mon/in_groups_of/groups_of.rb
+++ Sabrina/wk04/1-mon/in_groups_of/groups_of.rb
@@ -1,22 +1,45 @@
 =begin
 write a program in ruby that randomly organize a list of things into groups
-
 prompts the user for a list of things and store them inside an array
 prompts the user for a group size
 randomly assign things into groups with size given
 print out the groups in whatever way you like
+
 enter a list:
 bart,homer,lisa,maggie
 enter group size:
 2
-
 [['bart','lisa'],['maggie','homer']]
-if there are left overs with the given group size just assign them to the last group
 
+if there are left overs with the given group size just assign them to the last group
 enter a list:
 bart,homer,lisa,maggie,ned
 enter group size:
 2
-
 [['bart','lisa'],['ned','homer','maggie']]
 =end
+
+print "Enter a list: "
+list_array = gets.chomp
+print "Enter group size: "
+group_size = gets.chomp.to_i
+puts list_array.split(',')
+
+counter = 0
+new_array = []
+
+if list_array.length % group_size != 0
+  length = Math.floor (list_array.length / group_size)
+else
+  length = list_array.length / group_size
+end
+puts length
+
+length.times do
+  new_array.push (list_array.slice(counter, counter + group_size))
+  counter = counter + group_size
+  puts counter
+  puts new_array
+end
+
+puts new_array
